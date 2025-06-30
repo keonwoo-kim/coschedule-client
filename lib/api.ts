254ccee2fdea7d2695a1dfe1b013ed0f38e6fb11
keonwoo-kim <keonwoo.kim123@gmail.com>
2025-06-30 13:19:43 -0500

    Docker and deployment setup

--- lib/api.ts
+++ lib/api.ts
@@ -3,12 +3,21 @@ import Router from "next/router";
 import { useUserStore } from "@/store/useUserStore";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = useUserStore.getState().token;
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 api.interceptors.response.use(
-  res => res,
-  err => {
+  (res) => res,
+  (err) => {
     if (err?.response?.status === 401) {
       const userStore = useUserStore.getState();
       userStore.setToken(null);
